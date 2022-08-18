9633f661bb4407f1191c623b96f377e22746d618
Matija Sosic <matija.sosic@gmail.com>
2022-08-18 13:59:49 +0200

    moved update logic to Player file.

--- entities/Player.lua
+++ entities/Player.lua
@@ -32,4 +32,57 @@ function Player:draw()
     --love.graphics.rectangle('line', self:getRect())
 end
 
+function Player:update(dt, world, gravity)
+    local dx = 0
+    local dy = 0
+
+    if love.keyboard.isDown("right") then
+        dx = self.xMovSpeed * dt
+    end
+    if love.keyboard.isDown("left") then
+        dx = -self.xMovSpeed * dt
+    end
+    
+    -- Apply gravity
+    if not self.isGrounded then
+        self.yCurrVelocity = self.yCurrVelocity + gravity * dt
+    end
+
+    if love.keyboard.isDown('up') and self.isGrounded then
+        self.yCurrVelocity = -900
+        --self.isGrounded = false
+    end
+
+    dy = self.yCurrVelocity * dt
+
+    local goalX = self.x + dx
+    local goalY = self.y + dy
+
+    self.x, self.y, collisions, collLen = world:move(self, goalX, goalY)
+
+    -- If e.g. player walks or jumps off the platform, we want
+    -- him to start falling.
+    if collLen == 0 then
+        self.isGrounded = false
+    end
+
+    for i, coll in ipairs(collisions) do
+        if coll.touch.y > goalY then
+            -- Couldn't go as high as wanted - blocked from above
+            -- (e.g. when jumping, bumped head in the platform above).
+            self.yCurrVelocity = 0
+
+        -- NOTE(matija): Checking normal is important because want player to keep
+        -- falling if there was a side collision (e.g. against a wall). Normal
+        -- will be -1 in case of the floor collision.
+        elseif coll.normal.y < 0 then
+            -- Couldn't go as low as wanted - blocked from below (e.g. fell on the
+            -- ground).
+            self.isGrounded = true
+            self.yCurrVelocity = 0
+        end
+    end
+
+end
+
 return Player
--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ Player = require "entities.Player"
 Platform = require 'entities.Platform'
 
 local tileSize = 16
-local gravity = 900
+local gravity = 1900
 
 function love.load()
     math.randomseed( os.time() )
@@ -18,7 +18,7 @@ function love.load()
     camera:setFollowLerp(0.2)
     camera:setFollowStyle('PLATFORMER')
 
-    player = Player(300, 50)
+    player = Player(100, 50)
 
     -- Add some platforms
     entities = {player, unpack(generatePlatforms())}
@@ -36,58 +36,7 @@ function love.update(dt)
     camera:update(dt)
     camera:follow(player.x, player.y)
 
-    local dx = 0
-    local dy = 0
-
-    -- TODO(matija): extract this to player or provide interface for the player.
-    if love.keyboard.isDown("right") then
-        dx = player.xMovSpeed * dt
-    end
-    if love.keyboard.isDown("left") then
-        dx = -player.xMovSpeed * dt
-    end
-    
-    -- Apply gravity
-    if not player.isGrounded then
-        player.yCurrVelocity = player.yCurrVelocity + gravity * dt
-    end
-
-    if love.keyboard.isDown('up') and player.isGrounded then
-        player.yCurrVelocity = -600
-        --player.isGrounded = false
-    end
-
-    dy = player.yCurrVelocity * dt
-
-    local goalX = player.x + dx
-    local goalY = player.y + dy
-
-    player.x, player.y, collisions, collLen = world:move(player, goalX, goalY)
-
-    -- If e.g. player walks or jumps off the platform, we want
-    -- him to start falling.
-    if collLen == 0 then
-        player.isGrounded = false
-    end
-
-    for i, coll in ipairs(collisions) do
-        if coll.touch.y > goalY then
-            -- Couldn't go as high as wanted - blocked from above
-            -- (e.g. when jumping, bumped head in the platform above).
-            player.yCurrVelocity = 0
-
-        -- NOTE(matija): Checking normal is important because want player to keep
-        -- falling if there was a side collision (e.g. against a wall). Normal
-        -- will be -1 in case of the floor collision.
-        elseif coll.normal.y < 0 then
-            -- Couldn't go as low as wanted - blocked from below (e.g. fell on the
-            -- ground).
-            player.isGrounded = true
-            player.yCurrVelocity = 0
-        end
-
-        print ('normal', coll.normal.y)
-    end
+    player:update(dt, world, gravity)
 
 
     --print (player.x, player.y, len)
@@ -111,7 +60,7 @@ function love.draw()
 
     camera:detach()
 
-
+    -- HUD
     love.graphics.setColor(0, 0, 0)
     love.graphics.print("Score: xxx", 10, 10)
 end
@@ -132,7 +81,7 @@ function generatePlatforms()
     local maxXDist = 200
 
     local minYDist = 50
-    local maxYDist = 200
+    local maxYDist = 150
 
     local platforms = {}
 
