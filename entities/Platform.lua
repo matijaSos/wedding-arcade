bfa1b6d9361365f9f290421b21ef6f25a00155f7
Martin Sosic <sosic.martin@gmail.com>
2022-09-04 13:01:25 +0200

    Made platforms look like wedding tables.

--- entities/Platform.lua
+++ entities/Platform.lua
@@ -11,8 +11,30 @@ function Platform:init(x, y, w, h)
 end
 
 function Platform:draw()
-    love.graphics.setColor(1, 0.78, 0.15)
+    -- Draw table
+    love.graphics.setColor(0.95, 0.95, 0.95)
     love.graphics.rectangle('fill', self:getRect())
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle('line', self:getRect())
+    -- Draw legs
+    love.graphics.setColor(love.math.colorFromBytes(164, 116, 73))
+    local legWidth = 20
+    local legHeight = 40
+    local distFromLegToTableEnd = 20
+    love.graphics.rectangle(
+      'fill',
+      self.x + distFromLegToTableEnd,
+      self.y + self.h,
+      legWidth,
+      legHeight
+    )
+    love.graphics.rectangle(
+      'fill',
+      self.x + self.w - legWidth - distFromLegToTableEnd,
+      self.y + self.h,
+      legWidth,
+      legHeight
+    )
 end
 
 return Platform
